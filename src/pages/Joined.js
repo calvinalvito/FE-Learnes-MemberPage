03f746ec67af16249392b70a7201f2be190d2976
calvinalvito <calvinalvito19@gmail.com>
2023-01-17 16:26:00 +0700

    Rev Joined

--- src/pages/Joined.js
+++ src/pages/Joined.js
@@ -7,7 +7,7 @@ import { useHistory } from "react-router-dom";
 import ServerError from "pages/500";
 import Loading from "parts/Loading";
 
-const history = useHistory();
+
 export default function Joined({ history, match }) {
   const [state, setstate] = useState(() => ({
     isLoading: true,
@@ -26,7 +26,7 @@ export default function Joined({ history, match }) {
       if (error?.response?.data?.message === "user already take this course")
         history.push(`/courses/${match.params.class}`);
     }
-  }, [match.params.class]);
+  }, [match.params.class], history);
 
   useEffect(() => {
     joining();
